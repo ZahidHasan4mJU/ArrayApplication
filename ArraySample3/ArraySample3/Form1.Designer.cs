c5b7c7e95572daed2f2fad2f06a8f1a8a217d1a4
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-28 22:20:10 +0600

    finally added

---
+++ ArraySample3/ArraySample3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ArraySample3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterNameLabel = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.rankLabel = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.enterNameTextBox = new System.Windows.Forms.TextBox();
+            this.rankTextBox = new System.Windows.Forms.TextBox();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enterNameLabel
+            // 
+            this.enterNameLabel.AutoSize = true;
+            this.enterNameLabel.Location = new System.Drawing.Point(43, 24);
+            this.enterNameLabel.Name = "enterNameLabel";
+            this.enterNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.enterNameLabel.TabIndex = 0;
+            this.enterNameLabel.Text = "Enter Name";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(116, 163);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(174, 95);
+            this.listBox.TabIndex = 1;
+            // 
+            // rankLabel
+            // 
+            this.rankLabel.AutoSize = true;
+            this.rankLabel.Location = new System.Drawing.Point(43, 59);
+            this.rankLabel.Name = "rankLabel";
+            this.rankLabel.Size = new System.Drawing.Size(33, 13);
+            this.rankLabel.TabIndex = 2;
+            this.rankLabel.Text = "Rank";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(45, 95);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(35, 13);
+            this.countLabel.TabIndex = 3;
+            this.countLabel.Text = "Count";
+            // 
+            // enterNameTextBox
+            // 
+            this.enterNameTextBox.Location = new System.Drawing.Point(113, 21);
+            this.enterNameTextBox.Name = "enterNameTextBox";
+            this.enterNameTextBox.Size = new System.Drawing.Size(177, 20);
+            this.enterNameTextBox.TabIndex = 4;
+            // 
+            // rankTextBox
+            // 
+            this.rankTextBox.Location = new System.Drawing.Point(113, 58);
+            this.rankTextBox.Name = "rankTextBox";
+            this.rankTextBox.Size = new System.Drawing.Size(177, 20);
+            this.rankTextBox.TabIndex = 5;
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Location = new System.Drawing.Point(116, 91);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.Size = new System.Drawing.Size(174, 20);
+            this.countTextBox.TabIndex = 6;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(296, 21);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(87, 57);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(116, 134);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(174, 23);
+            this.showAllButton.TabIndex = 8;
+            this.showAllButton.Text = "ShowAllName";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(296, 91);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(87, 23);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 270);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.rankTextBox);
+            this.Controls.Add(this.enterNameTextBox);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.rankLabel);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.enterNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterNameLabel;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label rankLabel;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.TextBox enterNameTextBox;
+        private System.Windows.Forms.TextBox rankTextBox;
+        private System.Windows.Forms.TextBox countTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ ArraySample3/ArraySample3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraySample3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        string[] name = new string[10];
+        string[] rank = new string[10];
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if(enterNameTextBox.Text != "" && rankTextBox.Text != ""){
+                name[count] = enterNameTextBox.Text;
+                enterNameTextBox.Text = "";
+                rank[count] = rankTextBox.Text;
+                rankTextBox.Text = "";
+                count++;
+                countTextBox.Text = count.ToString();
+            }
+
+
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            int i = 0;
+            while (i < name.Length && i < rank.Length)
+            {
+                if(name[i] != null && rank[i] != null)
+                listBox.Items.Add(name[i] + "->" +rank[i]);
+                i++;
+            }
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            countTextBox.Text = "0";
+        }
+    }
+}
